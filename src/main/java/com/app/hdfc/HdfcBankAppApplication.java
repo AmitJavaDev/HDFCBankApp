9bf5346f37211a387086ad77648b1f4cb10827c5
AmitkrRana08 <se.amitkr.rana@gmail.com>
2022-09-07 15:24:41 +0530

    Added projet to repo

---
+++ src/main/java/com/app/hdfc/HdfcBankAppApplication.java
@@ -0,0 +1,14 @@
+package com.app.hdfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfcBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfcBankAppApplication.class, args);
+		System.out.println("In main................");
+	}
+
+}
---
+++ src/test/java/com/app/hdfc/HdfcBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hdfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfcBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
